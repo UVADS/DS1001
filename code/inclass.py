db53b1241455843dcdce56e3dbd9766d054b729e
Brian Wright <bwright.wright6@gmail.com>
2025-10-08 16:56:57 +0000

    add new envir

--- code/inclass.py
+++ code/inclass.py
@@ -9,7 +9,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.11.2
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: .venv
 #     language: python
 #     name: python3
 # ---
